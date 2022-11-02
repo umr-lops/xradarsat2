0e13b2cb09a1c0d0a869191fec7f6d240f4a0d98
yreynaud <yann.reynaud10@gmail.com>
2022-11-02 14:23:26 +0100

    creating repo

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
-from setuptools import setup, find_packages
-import glob
+from setuptools import setup
 
-setup(
-    name='radarSat2_xarray_reader',
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
-    scripts=glob.glob('src/scripts/*.py'),
-    url='https://github.com/umr-lops/radarSat2_xarray_reader',
-    use_scm_version=True,
-    setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
-    include_package_data=True,
-    install_requires=[
-        'xmltodict',
-        'glob',
-        'ast',
-        'numpy',
-        'os',
-        'xarray',
-        'datatree'
-    ],
-    license='MIT',
-    author='Reynaud Yann',
-    author_email='Yann.Reynaud.2@ifremer.fr',
-    description='xarray/dask distributed L1 sar file reader for radarSat2'
-)
+if __name__ == "__main__":
+    setup()
---
+++ src/radarSat2_xarray_reader/__init__.py
@@ -0,0 +1,1 @@
+from .radarSat2_xarray_reader import rs2_reader  # noqa: F401
