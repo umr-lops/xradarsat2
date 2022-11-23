0c00b7fa24cba2e771aedb637e875ffb59d5f9a8
yreynaud <yann.reynaud10@gmail.com>
2022-11-23 15:53:50 +0100

    Add load_digital_numbers to radarSat2_xarray_reader and dependencies to the toml file

--- src/xradarsat2/__init__.py
+++ src/xradarsat2/__init__.py
@@ -1,1 +1,2 @@
+from .radarSat2_xarray_reader import load_digital_number  # noqa: F401
 from .radarSat2_xarray_reader import rs2_reader  # noqa: F401
--- src/xradarsat2/radarSat2_xarray_reader.py
+++ src/xradarsat2/radarSat2_xarray_reader.py
@@ -2,11 +2,17 @@
 import ast
 import glob
 import os
+import re
 
+import dask
 import datatree
 import numpy as np
+import rasterio
+import rioxarray
 import xarray as xr
 import xmltodict
+import yaml
+from affine import Affine
 
 xpath_dict = {
     "geolocation_grid": {
@@ -1613,6 +1619,208 @@ def create_dataset_lut(files, dt, folder_path):
     return ds
 
 
+def list_tiff_files(root_path):
+    """
+    Return a list that contains all tiff files paths
+
+    ------------------------------------------------
+
+    :type root_path: str
+    :rtype: List[str]
+    :param root_path: Folder path containing the level 1 files
+    :return: List of Tiff file paths located in a folder
+    """
+
+    return glob.glob(os.path.join(root_path, "*imagery*tif"))
+
+
+def get_glob(strlist):
+    """
+    From list of str, replace diff by '?'
+
+    ------------------------------------------------
+
+    :rtype: str
+    :type strlist: List[str]
+    :param strlist: List of str
+    :return: replace diff between strings by `?`
+    """
+
+    def _get_glob(st):
+        stglob = "".join(
+            [
+                "?" if len(charlist) > 1 else charlist[0]
+                for charlist in [list(set(charset)) for charset in zip(*st)]
+            ]
+        )
+        return re.sub(r"\?+", "*", stglob)
+
+    strglob = _get_glob(strlist)
+    if strglob.endswith("*"):
+        strglob += _get_glob(s[::-1] for s in strlist)[::-1]
+        strglob = strglob.replace("**", "*")
+
+    return strglob
+
+
+def load_digital_number(
+    folder_path,
+    dt,
+    resolution=None,
+    chunks=None,
+    resampling=rasterio.enums.Resampling.rms,
+):
+    """
+    Add to the initial datatree (that contains all datasets) a new dataset with digital numbers
+    (name of the dataset = `digital_numbers`)
+
+    ------------------------------------------------
+
+    :rtype: datatree.Datatree
+    :type resampling: rasterio.enums.Resampling
+    :type chunks: dict[str, int]
+    :type resolution: str | dict[str, int] | None | number
+    :type dt: datatree.Datatree
+    :type folder_path: str
+    :param folder_path: Folder path containing the level 1 files
+    :param dt: datatree containing every dataset
+    :param resolution: Resampling dict like `{'line': 20, 'sample': 20}` where 20 is in pixels.
+        if a number, dict will be constructed from `{'line': number, 'sample': number}`
+        if str, it must end with 'm' (meters), like '100m'. dict will be computed from sensor pixel size
+    :param chunks: dict with keys ['pol','line','sample'] (dask chunks)
+    :param resampling: Only used if `resolution` is not None.
+        ` rasterio.enums.Resampling.rms` by default. `rasterio.enums.Resampling.nearest` (decimation) is fastest
+    :return: Initial datatree + dataset (possibly dual-pol), with basic coords/dims naming convention
+    """
+
+    tiff_files = list_tiff_files(folder_path)
+    map_dims = {"pol": "band", "line": "y", "sample": "x"}
+    if resolution is not None:
+        comment = 'resampled at "%s" with %s.%s.%s' % (
+            resolution,
+            resampling.__module__,
+            resampling.__class__.__name__,
+            resampling.name,
+        )
+    else:
+        comment = "read at full resolution"
+
+    # arbitrary rio object, to get shape, etc ... (will not be used to read data)
+    rio = rasterio.open(tiff_files[0])
+
+    chunks["pol"] = 1
+    # sort chunks keys like map_dims
+    chunks = dict(
+        sorted(chunks.items(), key=lambda pair: list(map_dims.keys()).index(pair[0]))
+    )
+    chunks_rio = {map_dims[d]: chunks[d] for d in map_dims.keys()}
+    if resolution is None:
+        # using tiff driver: need to read individual tiff and concat them
+        # riofiles['rio'] is ordered like self.s1meta.manifest_attrs['polarizations']
+
+        dn = xr.concat(
+            [
+                rioxarray.open_rasterio(f, chunks=chunks_rio, parse_coordinates=False)
+                for f in tiff_files
+            ],
+            "band",
+        ).assign_coords(
+            band=np.arange(len(dt["imageGenerationParameters"]["chirp"]["pole"].values))
+            + 1
+        )
+
+        # set dimensions names
+        dn = dn.rename(dict(zip(map_dims.values(), map_dims.keys())))
+
+        # create coordinates from dimension index (because of parse_coordinates=False)
+        dn = dn.assign_coords({"line": dn.line, "sample": dn.sample})
+        dn = dn.drop_vars("spatial_ref", errors="ignore")
+    else:
+        if not isinstance(resolution, dict):
+            if isinstance(resolution, str) and resolution.endswith("m"):
+                resolution = float(resolution[:-1])
+            resolution = dict(
+                line=resolution
+                / dt["geolocationGrid"]["line"].attrs[
+                    "rasterAttributes_sampledLineSpacing_value"
+                ],
+                sample=resolution
+                / dt["geolocationGrid"]["pixel"].attrs[
+                    "rasterAttributes_sampledPixelSpacing_value"
+                ],
+            )
+
+        # resample the DN at gdal level, before feeding it to the dataset
+        out_shape = (
+            int(rio.height / resolution["line"]),
+            int(rio.width / resolution["sample"]),
+        )
+        out_shape_pol = (1,) + out_shape
+        # read resampled array in one chunk, and rechunk
+        # this doesn't optimize memory, but total size remain quite small
+
+        if isinstance(resolution["line"], int):
+            # legacy behaviour: winsize is the maximum full image size that can be divided  by resolution (int)
+            winsize = (
+                0,
+                0,
+                rio.width // resolution["sample"] * resolution["sample"],
+                rio.height // resolution["line"] * resolution["line"],
+            )
+            window = rasterio.windows.Window(*winsize)
+        else:
+            window = None
+
+        dn = xr.concat(
+            [
+                xr.DataArray(
+                    dask.array.from_array(
+                        rasterio.open(f).read(
+                            out_shape=out_shape_pol,
+                            resampling=resampling,
+                            window=window,
+                        ),
+                        chunks=chunks_rio,
+                    ),
+                    dims=tuple(map_dims.keys()),
+                    coords={"pol": [pol]},
+                )
+                for f, pol in zip(
+                    tiff_files, dt["imageGenerationParameters"]["chirp"]["pole"].values
+                )
+            ],
+            "pol",
+        ).chunk(chunks)
+
+        # create coordinates at box center
+        translate = Affine.translation(
+            (resolution["sample"] - 1) / 2, (resolution["line"] - 1) / 2
+        )
+        scale = Affine.scale(
+            rio.width // resolution["sample"] * resolution["sample"] / out_shape[1],
+            rio.height // resolution["line"] * resolution["line"] / out_shape[0],
+        )
+        sample, _ = translate * scale * (dn.sample, 0)
+        _, line = translate * scale * (0, dn.line)
+        dn = dn.assign_coords({"line": line, "sample": sample})
+
+    # for GTiff driver, pols are already ordered. just rename them
+    dn = dn.assign_coords(pol=dt["imageGenerationParameters"]["chirp"]["pole"].values)
+
+    descr = "not denoised"
+    var_name = "digital_number"
+
+    dn.attrs = {
+        "comment": "%s digital number, %s" % (descr, comment),
+        "history": yaml.safe_dump(
+            {var_name: get_glob([p.replace(folder_path + "/", "") for p in tiff_files])}
+        ),
+    }
+    ds = dn.to_dataset(name=var_name)
+    dt["digital_numbers"] = datatree.DataTree(data=ds)
+    return dt
+
+
 def rs2_reader(folder_path):
     """
     Principal function of the reader, that create a datatree with all the product.xml and lut xml files dataset
@@ -1713,7 +1921,3 @@ def rs2_reader(folder_path):
     ds_lut = create_dataset_lut(list_lut_files(folder_path), dt, folder_path)
     dt["lut"] = datatree.DataTree(data=ds_lut)
     return dt
-
-
-# TODO : create doc to fill documentation automatically ( see example on github --> Antoine messages)
-# TODO : read tif images
