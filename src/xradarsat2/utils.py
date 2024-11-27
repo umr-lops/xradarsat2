87ca92e9e417b4de19d6773066f2f08bd047016e
grouazel <antoine.grouazel@ifremer.fr>
2024-11-27 12:22:53 +0100

    make the unit test be portable

--- src/xradarsat2/utils.py
+++ src/xradarsat2/utils.py
@@ -3,6 +3,10 @@ import logging
 import os
 import yaml
 import re
+import aiohttp
+import fsspec
+import warnings
+import zipfile
 
 
 def get_glob(strlist):
@@ -45,3 +49,68 @@ def load_config():
     stream = open(config_path, "r")
     conf = yaml.load(stream, Loader=yaml.CLoader)
     return conf
+
+def get_test_file(fname):
+    """
+    get test file from  https://cyclobs.ifremer.fr/static/sarwing_datarmor/xsardata/
+    file is unzipped and extracted to `config['data_dir']`
+
+    Parameters
+    ----------
+    fname: str
+        file name to get (without '.zip' extension)
+
+    Returns
+    -------
+    str
+        path to file, relative to `config['data_dir']`
+
+    """
+    config = {'data_dir': '/tmp'}
+
+    def url_get(url, cache_dir=os.path.join(config['data_dir'], 'fsspec_cache')):
+        """
+        Get fil from url, using caching.
+
+        Parameters
+        ----------
+        url: str
+        cache_dir: str
+            Cache dir to use. default to `os.path.join(config['data_dir'], 'fsspec_cache')`
+
+        Raises
+        ------
+        FileNotFoundError
+
+        Returns
+        -------
+        filename: str
+            The local file name
+
+        Notes
+        -----
+        Due to fsspec, the returned filename won't match the remote one.
+        """
+
+        if '://' in url:
+            with fsspec.open(
+                    'filecache::%s' % url,
+                    https={'client_kwargs': {'timeout': aiohttp.ClientTimeout(total=3600)}},
+                    filecache={'cache_storage': os.path.join(os.path.join(config['data_dir'], 'fsspec_cache'))}
+            ) as f:
+                fname = f.name
+        else:
+            fname = url
+
+        return fname
+
+    res_path = config['data_dir']
+    base_url = 'https://cyclobs.ifremer.fr/static/sarwing_datarmor/xsardata'
+    file_url = '%s/%s.zip' % (base_url, fname)
+    if not os.path.exists(os.path.join(res_path, fname)):
+        warnings.warn("Downloading %s" % file_url)
+        local_file = url_get(file_url)
+        warnings.warn("Unzipping %s" % os.path.join(res_path, fname))
+        with zipfile.ZipFile(local_file, 'r') as zip_ref:
+            zip_ref.extractall(res_path)
+    return os.path.join(res_path, fname)
